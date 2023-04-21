34d30c107327730e11dd9408ab9259a0f087ec0e
Marton Pal <h161409@stud.u-szeged.hu>
2023-04-21 18:59:55 +0200

    Login backend

--- Project/app/src/main/java/com/leafbank/LoginActivity.java
+++ Project/app/src/main/java/com/leafbank/LoginActivity.java
@@ -1,22 +1,35 @@
 package com.leafbank;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthEmailException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+
+import org.w3c.dom.Text;
+
+import java.util.concurrent.BlockingDeque;
 
 public class LoginActivity extends AppCompatActivity {
     EditText emailEditText;
     EditText passwordEditText;
-
+    TextView emailErrorText;
+    TextView passwordErrorText;
     private FirebaseAuth firebase;
 
 
@@ -26,10 +39,12 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
 
-        emailEditText = findViewById(R.id.emailEditText);
-        passwordEditText = findViewById(R.id.passwordEditText);
-
+        emailEditText = findViewById(R.id.login_emailEditText);
+        passwordEditText = findViewById(R.id.login_passwordEditText);
+        emailErrorText = findViewById(R.id.login_emailErrorTextView);
+        passwordErrorText = findViewById(R.id.login_passwordErrorTextView);
         firebase = FirebaseAuth.getInstance();
+
     }
 
     public void registerClick(View view) {
@@ -41,16 +56,53 @@ public class LoginActivity extends AppCompatActivity {
         String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
 
+        // isEmpty CHECK
+        if(!authDatas(new String[]{email, password}, new TextView[]{emailErrorText, passwordErrorText})) return;
+
 //        Log.i(this.getClass().getName(), "loginClick: "+username+" "+password);
-        firebase.signInWithEmailAndPassword(email, password)
-                .addOnSuccessListener(this, authResult -> {startHome();})
-                .addOnFailureListener(this, authResult -> {
-                    Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show();
-                });
+        firebase.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                startHome();
+                return;
+            }
+            try {
+                throw task.getException();
+            } catch (FirebaseAuthInvalidCredentialsException e) {
+                emailErrorText.setText("Nem létezik ilyen felhasználó!");
+            } catch (FirebaseAuthInvalidUserException e){
+                emailErrorText.setText("Nem létezik ilyen felhasználó!");
+            } catch (Exception e) {
+                Log.e(this.getClass().getName(), e.getClass() + e.getMessage());
+            }
+        });
+    }
+
+    public static boolean authDatas(String[] fields, TextView[] textViews) {
+        boolean accept = true;
+
+        // isEmpty
+        for (int i = 0; i < fields.length; i++) {
+            textViews[i].setText("");
+            if (fields[i].isEmpty()){
+                textViews[i].setText("Minden mezőt kötelező kitölteni!");
+                accept = false;
+            }
+        }
+        return accept;
     }
 
     public void startHome(){
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // USER Auth
+        firebase = FirebaseAuth.getInstance();
+        if (firebase.getCurrentUser() != null){
+            startHome();
+        }
     }
 }
--- Project/app/src/main/java/com/leafbank/RegisterActivity.java
+++ Project/app/src/main/java/com/leafbank/RegisterActivity.java
@@ -4,25 +4,35 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+
+import org.w3c.dom.Text;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText emailEditText;
     EditText usernameEditText;
     EditText passwordEditText;
     EditText passwordRepEditText;
-    EditText emailEditText;
+    TextView emailErrorText;
+    TextView usernameErrorText;
+    TextView passwordErrorText;
+    TextView passwordRepErrorText;
 
-    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +40,69 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         getSupportActionBar().hide();
 
-        usernameEditText = findViewById(R.id.usernameEditText);
-        passwordEditText = findViewById(R.id.passwordEditText);
-        passwordRepEditText = findViewById(R.id.passwordRepEditText);
-        emailEditText = findViewById(R.id.emailEditText);
+        emailEditText = findViewById(R.id.register_emailEditText);
+        usernameEditText = findViewById(R.id.register_usernameEditText);
+        passwordEditText = findViewById(R.id.register_passwordEditText);
+        passwordRepEditText = findViewById(R.id.register_passwordRepEditText);
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        emailErrorText = findViewById(R.id.register_emailErrorTextView);
+        usernameErrorText = findViewById(R.id.register_usernameErrorTextView);
+        passwordErrorText = findViewById(R.id.register_passwordErrorTextView);
+        passwordRepErrorText = findViewById(R.id.register_passwordRepErrorTextView);
+
+        firebase = FirebaseAuth.getInstance();
     }
 
     public void registerClick(View view) {
+        String email = emailEditText.getText().toString();
         String username = usernameEditText.getText().toString();
         String password = passwordEditText.getText().toString();
         String passwordRep = passwordRepEditText.getText().toString();
-        String email = emailEditText.getText().toString();
 
-        //TODO: Ellenőrzés
+        // isEmpty CHECK
+        if(!LoginActivity.authDatas(new String[]{email, username, password, passwordRep}, new TextView[]{emailErrorText, usernameErrorText, passwordErrorText, passwordRepErrorText})) return;
 
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnSuccessListener(this, authResult -> {Log.d(this.getClass().getName(), "Sikeres létrehozás");})
-                .addOnFailureListener(this, authResult -> {Log.d(this.getClass().getName(), "Sikertelen");});
+        // PASSWORD EQUAl CHECK
+        if (!password.equals(passwordRep)){
+            passwordRepErrorText.setText("Nem egyezik a két jelszó!");
+            return;
+        }
+
+        firebase.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()){
+                startHome();
+                return;
+            }
+
+            try {
+                throw task.getException();
+            } catch (FirebaseAuthWeakPasswordException e){
+                passwordErrorText.setText("Jelszónak minimum 6 karakternek kell lennie!");
+            } catch (FirebaseAuthUserCollisionException e){
+                emailErrorText.setText("Az email cím már foglalt!");
+            } catch (Exception e) {
+                Log.e(this.getClass().getName(), e.getClass() + " " + e.getMessage());
+            }
+        });
     }
+
+    public void loginClick(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // USER Auth
+        if (firebase.getCurrentUser() != null){
+            startHome();
+        }
+    }
+
+    public void startHome(){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+
 }
